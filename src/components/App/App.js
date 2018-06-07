d809565d166ac019ee3561ac1025e2f159d8151d
Deepanshu Kumar <deepanshu@rkmarketing.net>
2018-06-07 17:10:10 +0530

    add up more test cases and modified test command

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,7 +4,6 @@ import './App.css';
 class App extends Component {
 
     constructor(props) {
-
         super(props);
         this.state = {
             emailErrorMessage: false,
@@ -45,15 +44,31 @@ class App extends Component {
     }
 
     handleChangeEmail = (e) => {
-        console.log(e.target.value);
         this.chkEmailFormat(e.target.value);
         this.setState({emailVal: e.target.value});
     }
 
 
     handleChangePhone = (e) => {
-        console.log(e.target.value);
         this.setState({phoneVal: e.target.value});
+    }
+
+    handleSubmit = () => {
+        if(this.state.emailVal === ""){
+            this.setState({
+                emailErrorMessage : true
+            })
+        } else if(this.state.phoneVal === ""){
+            this.setState({
+                phoneErrorMessage: true
+            })
+        } else if(this.state.dobVal === ""){
+            this.setState({
+                dobErrorMessage: true
+            })
+        } else {
+            console.log("Form Submitted Successfully!!!");
+        }
     }
 
 
@@ -68,54 +83,53 @@ class App extends Component {
 
             <div className="App">
                 <header className="App-header">
-
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
                 <form>
-                    <table width="30%" align="center">
+                    <table width="50%" align="center">
                         <tbody>
-                        <tr>
+                        <tr width="15%">
                             <td align="left">
                                 <label>Email:</label>
                             </td>
                             <td align="left">
                                 <input id="emailId" value={this.state.emailVal} onChange={this.handleChangeEmail} className="emailCls" type="email" name="email" required />
                             </td>
-                            <td >
-                                {this.state.emailErrorMessage ? <span id="emailErrorId">
+                            <td align="left">
+                                {this.state.emailErrorMessage ? <span id="emailErrorId" style={{color: 'red'}}>
                                     Please enter valid email
                                 </span> : '' }
                             </td>
                         </tr>
-                        <tr>
+                        <tr width="15%">
                             <td align="left">
                                 <label>Phone:</label>
                             </td>
                             <td align="left">
                                 <input id="phoneId" value={this.state.phoneVal} onChange={this.handleChangePhone} className="phoneCls" type="text" name="phone" pattern="[789][0-9]{9}" required />
                             </td>
-                            <td>
-                                {this.state.phoneErrorMessage ? <span id="phoneErrorId">
+                            <td align="left">
+                                {this.state.phoneErrorMessage ? <span id="phoneErrorId" style={{color: 'red'}}>
 
                                 </span> : null}
                             </td>
                         </tr>
-                        <tr>
+                        <tr width="20%">
                             <td align="left">
                                 <label>Date Of Birth:</label>
                             </td>
                             <td align="left">
                                 <input id="dobId" value={this.state.dobVal} onChange={this.handleChangeDob}  className="dobCls"  type="date" name="dob" required />
                             </td>
-                            <td>
-                                {this.state.dobErrorMessage ? <span id="dobErrorId">
+                            <td align="left">
+                                {this.state.dobErrorMessage ? <span id="dobErrorId" style={{color: 'red'}}>
                                     Please enter valid date.
                                 </span> : null }
                             </td>
                         </tr>
                         <tr>
                             <td colSpan="2" align="center">
-                                <input id="submitId" type="submit" name="submit" value="Submit" />
+                                <input id="submitId" type="button" name="submit" onClick={this.handleSubmit} value="Submit" />
                             </td>
                         </tr>
                         </tbody>
--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -28,10 +28,33 @@ describe('App', () => {
         expect(wrapper.find('#submitId').length).toEqual(1);
     });
 
+    it('Contains submit as button for id submitId', () => {
+        wrapper = shallow(<App />);
+        expect(wrapper.find('#submitId').props().type).toEqual('button');
+    });
+
     it('Contains simulated email value', () => {
         wrapper = shallow(<App />);
         wrapper.find('#emailId').simulate('change', {target: {value: "deep@mail.com"}});
         expect(wrapper.state().emailVal).toEqual('deep@mail.com');
+    });
+
+    it('contains email error span if wrong format of email found', () => {
+        wrapper = shallow(<App />);
+        wrapper.find('#emailId').simulate('change', {target: {value: "deep"}});
+        expect(wrapper.find('#emailErrorId').length).toEqual(1);
+    });
+
+    it('contains email error span if wrong format of email found', () => {
+        wrapper = shallow(<App />);
+        wrapper.find('#emailId').simulate('change', {target: {value: "deep@"}});
+        expect(wrapper.find('#emailErrorId').length).toEqual(1);
+    });
+
+    it('contains email error span if wrong format of email found', () => {
+        wrapper = shallow(<App />);
+        wrapper.find('#emailId').simulate('change', {target: {value: "deep@mail."}});
+        expect(wrapper.find('#emailErrorId').length).toEqual(1);
     });
 
     it('Contains simulated phone number value', () => {
@@ -44,6 +67,33 @@ describe('App', () => {
         wrapper = shallow(<App />);
         wrapper.find('#phoneId').simulate('change', {target: {value: "2018-06-06"}});
         expect(wrapper.state().phoneVal).toEqual('2018-06-06');
+    });
+
+    it('contains validation check for future date', () => {
+        wrapper = shallow(<App />);
+        wrapper.find('#dobId').simulate('change', {target: {value: "2099-08-08"}});
+        expect(wrapper.find('#dobErrorId').length).toEqual(1);
+    });
+
+    it('called handleSubmit on click of submit button', () => {
+        wrapper = shallow(<App />);
+        wrapper.find('#submitId').prop('onClick')();
+        expect(wrapper.state().emailErrorMessage).toEqual(true);
+    });
+
+    it('fill email value and called handleSubmit on click of submit button and shows error span for phone number', () => {
+        wrapper = shallow(<App />);
+        wrapper.find('#emailId').simulate('change', {target: {value: "deep@mail.com"}});
+        wrapper.find('#submitId').prop('onClick')();
+        expect(wrapper.state().phoneErrorMessage).toEqual(true);
+    });
+
+    it('fill email and phone number value and called handleSubmit on click of submit button and shows error span for DOB', () => {
+        wrapper = shallow(<App />);
+        wrapper.find('#emailId').simulate('change', {target: {value: "deep@mail.com"}});
+        wrapper.find('#phoneId').simulate('change', {target: {value: "9876543210"}});
+        wrapper.find('#submitId').prop('onClick')();
+        expect(wrapper.state().dobErrorMessage).toEqual(true);
     });
 
 });
